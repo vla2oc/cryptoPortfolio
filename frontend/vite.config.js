a2c7b6dcb43c146260cde6bf8cdb992042d32063
vla2oc <kurochka265@gmail.com>
2026-06-05 19:45:55 +0200

    clean files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base: "/cryptoPortfolio",
   server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
+    host: true, // Это равносильно 0.0.0.0 (слушать всех)
+    port: 5173, // Жестко задаем порт
+    strictPort: true, // Если порт занят, Vite упадет (лучше, чем сменит порт втихую)
     watch: {
-      usePolling: true,
+      usePolling: true, // Для Windows/Mac иногда нужно, чтобы работало автообновление
     },
   },
 });
