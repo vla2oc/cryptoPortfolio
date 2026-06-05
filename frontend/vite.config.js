1218294d8588831d2766806f5d6ffa099d978ec9
vla2oc <kurochka265@gmail.com>
2026-06-05 19:31:13 +0200

    clean files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  //base: '/cryptoPortfolio',
+  base: "/",
   server: {
-    host: true, // Это равносильно 0.0.0.0 (слушать всех)
-    port: 5173, // Жестко задаем порт
-    strictPort: true, // Если порт занят, Vite упадет (лучше, чем сменит порт втихую)
+    host: true,
+    port: 5173,
+    strictPort: true,
     watch: {
-      usePolling: true, // Для Windows/Mac иногда нужно, чтобы работало автообновление
+      usePolling: true,
     },
   },
 });
--- src/utils.js
+++
@@ -1,7 +0,0 @@
-export function percentDifference(a, b){
-    return +(((b - a) / Math.abs(a)) * 100).toFixed(2);
-}
-
-export function capitalize(str){
-    return str.charAt(0).toUpperCase() + str.substr(1)
-}
